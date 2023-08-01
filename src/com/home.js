8526502d78d71b12fe32b0c32b9e02ec2dc8ccb4
abdirahiim ahmed <itsabdirahiim@gmail.com>
2023-08-01 12:53:22 +0200

    #

--- src/com/home.js
+++ src/com/home.js
@@ -16,6 +16,7 @@ export default function Home() {
           return response.json();
         } else {
           throw new Error("Unauthorized");
+          
         }
       })
       .then((data) => {
