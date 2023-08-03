1f00de142cf6590f6781c69ca1f2dc92cd83f583
abdirahiim ahmed <itsabdirahiim@gmail.com>
2023-08-03 17:42:56 +0200

    #

--- src/com/notes.js
+++ src/com/notes.js
@@ -32,7 +32,7 @@ export default function Notes() {
         setApiData(data);
       })
       .catch((error) => {
-        window.location.href = "/login";
+        window.location.href = "/";
       });
   }, []);
 
